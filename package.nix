8fc973257c789540f8a2b79950caea7dc8359926
HoshinoTented <hoshinotented@qq.com>
2026-06-19 08:35:50 +0800

    pkg: fix v1.1.15

--- package.nix
+++ package.nix
@@ -18,12 +18,14 @@
   gcc,
   glibc,
   alsa-lib,
+  libayatana-appindicator,
 }:
 
 let
-  version = "1.1.10";
+  appName = "prismterminal-tauri";
+  version = "1.1.15";
   release = "https://github.com/Kagami-Studio/PrismTerminal-Release/releases/download/v${version}/PrismTerminal-v${version}-linux64.deb";
-  hash = "sha256-91sdPjLow5StiuHNdO9U0+6vJliQLr5DpHCL0XEp0G4=";
+  hash = "sha256-q6j9UxDodpADK42rzlDeKthtesg7GuI24yAjcU/xuuI=";
 in
 
 stdenv.mkDerivation {
@@ -58,17 +60,21 @@ stdenv.mkDerivation {
     alsa-lib
   ];
 
+  # https://github.com/NixOS/nixpkgs/blob/nixos-26.05/pkgs/build-support/setup-hooks/auto-patchelf.sh
+  runtimeDependencies = [ libayatana-appindicator ];
+
   installPhase = ''
     runHook preInstall
 
     cd usr/
-    install -Dm755 ./bin/prismterminal-tauri $out/bin/prismterminal-tauri
+    install -Dm755 ./bin/${appName} $out/bin/${appName}
     find ./share -type f -exec sh -c 'FILE="$1"; install -Dm755 "$FILE" $out/"$FILE"' sh {} \;
 
     runHook postInstall
   '';
 
   meta = with lib; {
+    mainProgram = appName;
     homepage = "https://kagamistudio.com/";
     description = "The next generation of meowpad configurator";
     platforms = [ "x86_64-linux" ];
